0385bfa7a2df5708217246fb127fe412e7b87639
dustinengle <dustin.engle@gmail.com>
2018-04-18 10:09:50 -0700

    setup vendor folder and start on qr code integration

--- client/src/component/Donate.jsx
+++ client/src/component/Donate.jsx
@@ -1,19 +1,37 @@
 
 const PropTypes = require('prop-types');
+const QRCode = require('qrcode');
 const React = require('react');
 
 const Card = require('../ui/Card');
 
-const Donate = (props) => (
-  <div className="donate">
-    <Card title="Donate Address">
-      Donate QR: { props.donationAddress }
-    </Card>
-  </div>
-);
+class Donate extends React.Component {
+  static propTypes = {
+    donationAddress: PropTypes.string.isRequired
+  };
 
-Donate.propTypes = {
-  donationAddress: PropTypes.string.isRequired
-};
+  constructor(props) {
+    super(props);
+    this.qrcode = null;
+  };
+
+  componentDidMount() {
+    this.qrcode = new QRCode(
+      document.getElementById('donate-qr'),
+      this.props.donationAddress
+    );
+  };
+
+  render() {
+    return (
+      <div className="donate">
+        <Card title="Donate Address">
+          Donate QR:<br />
+          <div id="donate-qr" />
+        </Card>
+      </div>
+    );
+  };
+}
 
 module.exports = Donate;
