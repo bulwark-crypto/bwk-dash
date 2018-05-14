95ab61e558f7eb43fb6b7775fe4ad9452d340333
dustinengle <dustin.engle@gmail.com>
2018-05-14 13:02:01 -0700

    updated build script to include new binaries

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 echo "Buidling BWK-Dash..."
 go build -o $GOPATH/bin/bwk-dash $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-dash/*.go
+
+echo "Buidling BWK-Clean..."
+go build -o $GOPATH/bin/bwk-clean $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-clean/*.go
+
+echo "Buidling BWK-Cron..."
+go build -o $GOPATH/bin/bwk-cron $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-cron/*.go
+
 echo "Finished!"
