c90bc3166d8b0ce1f0557d0d6000a218f394d7d9
dustinengle <dustin.engle@gmail.com>
2018-05-14 11:58:23 -0700

    remove rpc request code

--- handler/handler.go
+++ handler/handler.go
@@ -1,60 +1,15 @@
 package handler
 
 import (
-	"github.com/dustinengle/bwk-dash/rpc"
-	"github.com/dustinengle/bwk-dash/sys"
 	"github.com/gin-gonic/gin"
 )
 
 // GetNodeInfo will return all the information
 // needed to be displayed on the dashboard.
 func GetNodeInfo(c *gin.Context) {
-	// Get node rpc connection from request context.
-	node := c.MustGet("rpc").(*rpc.RPC)
-
-	// Get node information.
-	info, err := node.GetInfo()
-	if err != nil {
-		c.Error(err)
-		return
-	}
-
-	// Setup the response object.
-	res := new(InfoResponse)
-	res.Blocks = info.Result.Blocks
-	res.Blocks = info.Result.Blocks
-	res.Connections = info.Result.Connections
-	res.Country = ""
-	res.Difficulty = info.Result.Difficulty
-	res.DonationAddress = c.MustGet("donation").(string)
-	res.IP = c.MustGet("ip").(string)
-	res.MaxMemory = sys.GetMemorySize()
-	res.NetworkHashPS = node.GetNetworkHashPS()
-	res.Protocol = info.Result.Protocol
-	res.Rank = 0 // TODO: get masternode rank if masternode.
-	res.Status = "Online"
-	res.StakingStatus = info.Result.StakingStatus
-	res.Subversion = node.GetVersion()
-	res.Transactions = node.GetTransactions()
-	res.UsedMemory = 0
-	res.Version = info.Result.Version
-
-	// Setup the network name.
-	res.Network = "mainnet"
-	if info.Result.Testnet {
-		res.Network = "testnet"
-	}
-
-	// Get the network traffic.
-	res.IncomingData, res.OutgoingData = node.GetNetTotals()
-	res.TotalData = res.IncomingData + res.OutgoingData
-
-	// Get the max, mid, and min fees.
-	res.MaxFee, res.MidFee, res.MinFee = node.GetFees()
-
-	// Get memory information for the node.
-	res.UsedMemory = node.GetUsedMemory()
+	// Get DB connection.
+	// Pull last entry and return.
 
 	// Return as json to client.
-	c.JSON(200, res)
+	c.JSON(200, "")
 }
