94dc8b451e8b63b8a73db7307e95d55d8390e572
dustinengle <dustin.engle@gmail.com>
2018-05-21 13:56:44 -0700

    updated build script

--- script/build.sh
+++ script/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Moving to source folder..."
+cd $GOPATH/src/github.com/dustinengle/bwk-dash
+
 echo "Buidling BWK-Dash..."
-go build -o $GOPATH/bin/bwk-dash $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-dash/*.go
+go build -o $GOPATH/bin/bwk-dash cmd/bwk-dash/*.go
 
 echo "Buidling BWK-Clean..."
-go build -o $GOPATH/bin/bwk-clean $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-clean/*.go
+go build -o $GOPATH/bin/bwk-clean cmd/bwk-clean/*.go
 
 echo "Buidling BWK-Cron..."
-go build -o $GOPATH/bin/bwk-cron $GOPATH/src/github.com/dustinengle/bwk-dash/cmd/bwk-cron/*.go
+go build -o $GOPATH/bin/bwk-cron cmd/bwk-cron/*.go
 
 echo "Finished!"
