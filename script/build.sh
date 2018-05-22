4d70acbe78df33ad7b7673c56253343c94c6b970
dustinengle <dustin.engle@gmail.com>
2018-05-22 11:11:17 -0700

    updated to cross-compile arm using gcc

--- script/build.sh
+++ script/build.sh
@@ -5,8 +5,8 @@ mkdir bin
 
 echo "Building:"
 
-GOOS=linux GOARCH=arm go build -o bin/linux-arm/bwk-cron cmd/bwk-cron/*.go
-GOOS=linux GOARCH=arm go build -o bin/linux-arm/bwk-dash cmd/bwk-dash/*.go
+GOOS=linux GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabi-gcc go build -o bin/linux-arm/bwk-cron cmd/bwk-cron/*.go
+GOOS=linux GOARCH=arm CGO_ENABLED=1 CC=arm-linux-gnueabi-gcc go build -o bin/linux-arm/bwk-dash cmd/bwk-dash/*.go
 echo "- Linux ARM done!"
 
 GOOS=linux GOARCH=amd64 go build -o bin/linux-amd64/bwk-cron cmd/bwk-cron/*.go
