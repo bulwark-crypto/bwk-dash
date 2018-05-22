b212bfb3778b491cba7ace6e8ac6a06ca24e5bee
dustinengle <dustin.engle@gmail.com>
2018-05-22 12:13:14 -0700

    updated package script to include html

--- script/pkg.sh
+++ script/pkg.sh
@@ -8,3 +8,6 @@ tar -zcf ../../build/bwk-dash-1.0.0-linux-arm.tar.gz bwk-cron bwk-dash
 
 cd ../linux-amd64
 tar -zcf ../../build/bwk-dash-1.0.0-linux-amd64.tar.gz bwk-cron bwk-dash
+
+cd ../../client/build
+tar -zcf ../../build/bwk-dash-html.tar.gz *
