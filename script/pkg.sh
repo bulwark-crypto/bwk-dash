ab3248196a038742b7c0b7ec8426ba8bd26418ff
dustinengle <dustin.engle@gmail.com>
2018-05-22 11:18:09 -0700

    updated pkg script

--- script/pkg.sh
+++ script/pkg.sh
@@ -3,5 +3,5 @@
 rm -fR build
 mkdir build 
 
-tar -zcf build/bwk-dash-1.0.0-linux-arm.tar.gz bin/linux-arm/*
-tar -zcf build/bwk-dash-1.0.0-linux-amd64.tar.gz bin/linux-amd64/*
+tar -zcf build/bwk-dash-1.0.0-linux-arm.tar.gz bin/linux-arm/bwk-cron bin/linux-arm/bwk-dash
+tar -zcf build/bwk-dash-1.0.0-linux-amd64.tar.gz bin/linux-amd64/bwk-cron bin/linux-amd64/bwk-dash
