c7330ee923602af5a904223a1e63eea15f2ad6ab
dustinengle <dustin.engle@gmail.com>
2018-05-23 16:27:38 -0700

    updated header

--- client/src/comp/Header.jsx
+++ client/src/comp/Header.jsx
@@ -4,32 +4,36 @@ const React = require('react');
 const Logo = require('../ui/Logo');
 
 const Header = () => (
-  <div className="header bx--row">
-    <div className="bx--col-sm-12 bx--col-md-2">
-      <Logo />
-    </div>
-    <div className="bx--col-sm-12 bx--col-md-4 bx--type-beta">
-      Home Node Dashboard
-    </div>
-    <div className="bx--col-sm-12 bx--col-md-6">
-      <a
-        className="rectangle-2"
-        href="#"
-        target="_blank">
-        Help
-      </a>
-      <a
-        className="rectangle-2"
-        href="https://explorer.bulwarkcrypto.com"
-        target="_blank">
-        Block Explorer
-      </a>
-      <a
-        className="rectangle-2"
-        href="https://github.com/bulwark-crypto"
-        target="_blank">
-        GitHub
-      </a>
+  <div className="header">
+    <div className="bx--grid">
+      <div className="bx--row">
+        <div className="bx--col-sm-12 bx--col-md-2">
+          <Logo />
+        </div>
+        <div className="bx--col-sm-12 bx--col-md-4 bx--type-beta">
+          Home Node Dashboard
+        </div>
+        <div className="bx--col-sm-12 bx--col-md-6" style={{ textAlign: 'right' }}>
+          <a
+            className="rectangle-2"
+            href="#"
+            target="_blank">
+            Help
+          </a>
+          <a
+            className="rectangle-2"
+            href="https://explorer.bulwarkcrypto.com"
+            target="_blank">
+            Block Explorer
+          </a>
+          <a
+            className="rectangle-2"
+            href="https://github.com/bulwark-crypto"
+            target="_blank">
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 );
